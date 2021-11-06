38fbf66ce217d79e1f1eef96f5d77ef7b1a86d3e
JavanAppleby <styx_n_stonz@hotmail.com>
2021-11-06 16:59:45 +1000

    updated index.js and index.html

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-let cr = 0.25
-let baseUrl = `https://www.dnd5eapi.co`
-var apiUrl = `${baseUrl}/api/monsters?challenge_rating=${cr}`
+// let baseUrl = `https://www.dnd5eapi.co`
+// let apiUrl = `${baseUrl}/api/monsters?challenge_rating=${cr}`
 let monsterArray
 const monsterDetails = {}
 
+document.getElementById("submit").addEventListener('click', (event) => {
+    event.preventDefault()
+    document.getElementById("user-cr").innerHTML = `Displaying Monsters with CR ${cr}`;
+  });
+
+function getCr () {
+    var cr = document.getElementById("cr").value;
+    let baseUrl = `https://www.dnd5eapi.co`
+    var apiUrl = `${baseUrl}/api/monsters?challenge_rating=${cr}`
+    return apiUrl
+}
+
+
 const fetchMonsters = async () => {
+
     const response = await fetch(apiUrl);
     const monsterData = await response.json();
     console.log(monsterData);
