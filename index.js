e94575c6fc710f3dbbe205d9c419a84b862a047f
JavanAppleby <styx_n_stonz@hotmail.com>
2021-11-10 17:11:45 +1000

    added corrections to index.js

--- index.js
+++ index.js
@@ -2,51 +2,50 @@ let monsterArray
 const monsterDetails = {}
 const baseUrl = "https://www.dnd5eapi.co/api/"
 
-document.getElementById("challengeRating").addEventListener('click', function() {
+document.getElementById("challengeRatings").addEventListener('change', function(event) {
     event.preventDefault();
     let cr = document.getElementById("challengeRatings").value;
-    let monsterUrl = `${baseUrl}/monsters?challenge_rating=${cr}`
-    
-    const fetchMonsters = async () => {
-        const response = await fetch(`${monsterUrl}`);
-        const monsterData = await response.json();
-        return monsterData;
-    }
-    
-    const mapMonsterData = async () => {
-        const data = await fetchMonsters();
-        data => {
-            monsterArray = data.results
-            
-            monsterArray.forEach(monster => {
-                if (!monsterDetails[monster.name]) {
-                    fetchMonsterDetails(monster)
-                }
-            });
-        }
-    }
-    
-    const fetchMonsterDetails = monster => {
-        return fetch(monster.url)
-        .then(res => res.json())
-        .then(data => {
-            monsterDetails[monster.name] = data
-            const card = generateMonsterCard(monsterDetails[monster.name])
-            document.querySelector('section.monster-list').append(card)
-        })
-    }
-    
-    
-    const generateMonsterCard = () => {
-        const card = document.createElement('div')
-        card.className = "monster-card"
-        
-        const alphabet = monsterDetails[monster.name].charAt(0);
-        card.innerHTML = `
-        <h4>CR ${cr} Monsters</h4>
-        `
-        return card
-    }
-    
-    mapMonsterData();
+    getMonsterWithCr(cr);
 })
+
+function getMonsterWithCr (cr) {
+    let monsterUrl = `${baseUrl}/monsters?challenge_rating=${cr}`
+    mapMonsterData(monsterUrl);
+}
+
+const fetchMonsters = async (monsterUrl) => {
+    const response = await fetch(`${monsterUrl}`);
+    const monsterData = await response.json();
+    return monsterData;
+}
+
+const mapMonsterData = async (monsterUrl) => {
+    const data = await fetchMonsters(monsterUrl);
+    data.results.forEach(monster => {
+        if (!monsterDetails[monster.name]) {
+            fetchMonsterDetails(monster)
+        }
+    });
+}
+
+const fetchMonsterDetails = async monster => {
+    const monsterListUrl = `${baseUrl}${monster.url}`
+    const response = await fetch(monsterListUrl)
+    const data = await response.json()
+    monsterDetails[monster.name] = data
+    const listItem = generateMonsterListItem(monsterDetails[monster.name])
+    document.querySelector('section.monster-list ul').append(listItem)
+}
+
+const generateMonsterListItem = (monsterName) => {
+    const listItem = document.createElement('li')
+    listItem.className = "monster-listByCr"
+    
+    const alphabet = monsterDetails[monster.name].charAt(0);
+    console.log(alphabet)
+    listItem.innerHTML = `
+    <h4>CR ${cr} Monsters</h4>
+        <h5>${alphabet}</h5>
+    `
+    return listItem
+}
